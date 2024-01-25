13cb287279ceb6212ab5979fd0d982154df3c405
Kenechukwu Akubue <kengoon19@gmail.com>
2024-01-25 17:44:42 +0100

    Add firestore listeners

---
+++ sjfirebase/jinterface/firestore.py
@@ -0,0 +1,27 @@
+from jnius import PythonJavaClass, java_method
+
+__all__ = ("EventListener", "OnProgressListener")
+
+
+class EventListener(PythonJavaClass):
+    __javainterfaces__ = ["com/google/firebase/firestore/EventListener"]
+    __javacontext__ = "app"
+
+    def __init__(self, on_event):
+        self.on_event = on_event
+
+    @java_method("(Ljava/lang/Object;com/google/firebase/firestore/FirebaseFirestoreException;)V")
+    def onEvent(self, value, error):
+        self.on_event(value, error)
+
+
+class OnProgressListener(PythonJavaClass):
+    __javainterfaces__ = ["com/google/firebase/firestore/OnProgressListener"]
+    __javacontext__ = "app"
+
+    def __init__(self, on_progress):
+        self.on_progress = on_progress
+
+    @java_method("(Ljava/lang/Object;)V")
+    def onProgress(self, snapshot):
+        self.on_progress(snapshot)
