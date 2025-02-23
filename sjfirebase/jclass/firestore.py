3385a74e3e80cda734c2ca3cad424ef20747063a
Kenechukwu Akubue <kengoon19@gmail.com>
2025-02-24 00:37:31 +0100

    update firestore module

--- sjfirebase/jclass/firestore.py
+++ sjfirebase/jclass/firestore.py
@@ -1,9 +1,35 @@
-from jnius import JavaClass, MetaJavaClass, JavaStaticMethod
+__all__ = ("SJFirebaseFirestore", "Source")
+
+from jnius import JavaClass, MetaJavaClass, JavaStaticMethod, JavaMultipleMethod
 from sjfirebase import package_path
 
-__all__ = ("SJFirebaseFirestore", )
+_Source_package = "com/google/firebase/firestore/Source"
+_Source_signature = "Lcom/google/firebase/firestore/Source;"
+_FieldValue_package = "com/google/firebase/firestore/FieldValue"
 
 
 class SJFirebaseFirestore(JavaClass, metaclass=MetaJavaClass):
     __javaclass__ = f"{package_path}SJFirebaseFirestore"
     get_db = JavaStaticMethod("()Lcom/google/firebase/firestore/FirebaseFirestore;")
+
+
+class Source(JavaClass, metaclass=MetaJavaClass):
+    __javaclass__ = _Source_package
+    CACHE = JavaStaticMethod(_Source_signature)
+    SERVER = JavaStaticMethod(_Source_signature)
+    DEFAULT = JavaStaticMethod(_Source_signature)
+    values = JavaStaticMethod(f"()[{_Source_signature}")
+    valueOf = JavaStaticMethod(f"(Ljava/lang/String;){_Source_signature}")
+
+
+class FieldValue(JavaClass, metaclass=MetaJavaClass):
+    __javaclass__ = _FieldValue_package
+    delete = JavaStaticMethod(f"(){_FieldValue_package};")
+    serverTimestamp = JavaStaticMethod(f"(){_FieldValue_package};")
+    arrayUnion = JavaStaticMethod(f"([Ljava/lang/Object;){_FieldValue_package};")
+    arrayRemove = JavaStaticMethod(f"([Ljava/lang/Object;){_FieldValue_package};")
+    increment = JavaMultipleMethod([
+        (f"(J){_FieldValue_package};", True, False),
+        (f"(D){_FieldValue_package};", True, False)
+    ])
+
